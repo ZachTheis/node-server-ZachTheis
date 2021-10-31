5c82c89426078c1901bbb619de212363c29c6213
ZachTheis <70988581+ZachTheis@users.noreply.github.com>
2021-10-31 19:59:19 -0400

    Add status codes
    Change status codes to match routing. Delete extraneous code.

--- server07.js
+++ server07.js
@@ -4,23 +4,21 @@ const fs = require('fs')
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-    /* console.log(req.url, req.method); */
-    res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    /* res.write('Hello World');
-    res.write('Hello World again')
-    res.end('<h1>Hello World</h1>'); */
 
     let path = './';
     switch(req.url) {
         case '/':
             path += 'index.html';
+            res.statusCode = 200;
             break;
         case '/about':
             path += 'about.html';
+            res.statusCode = 200;
             break;
         default:
             path += '404.html';
+            res.statusCode = 404;
             break;
     }
 
