53ed223c850fa34d3a363049a4f503b83bd1f3a7
ZachTheis <70988581+ZachTheis@users.noreply.github.com>
2021-10-31 19:56:57 -0400

    Add routing
    Add /about and 404 routing to server

--- server07.js
+++ server07.js
@@ -4,12 +4,27 @@ const fs = require('fs')
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
+    /* console.log(req.url, req.method); */
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     /* res.write('Hello World');
     res.write('Hello World again')
     res.end('<h1>Hello World</h1>'); */
-    fs.readFile('./index.html', (err, data) => {
+
+    let path = './';
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        default:
+            path += '404.html';
+            break;
+    }
+
+    fs.readFile(path, (err, data) => {
         if(err) {
             console.error(err);
             res.end()
