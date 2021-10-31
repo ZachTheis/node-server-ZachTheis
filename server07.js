65af8d704c0fe302a5cd11c56fa3e7bbcb798051
ZachTheis <70988581+ZachTheis@users.noreply.github.com>
2021-10-31 19:38:29 -0400

    Add server with html
    Create basic server with html files

--- server07.js
+++ server07.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs')
 
 const PORT = process.env.PORT || 3000;
 
@@ -6,8 +7,17 @@ const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     /* res.write('Hello World');
-    res.write('Hello World again') */
-    res.end('<h1>Hello World</h1>');
+    res.write('Hello World again')
+    res.end('<h1>Hello World</h1>'); */
+    fs.readFile('./index.html', (err, data) => {
+        if(err) {
+            console.error(err);
+            res.end()
+        } else {
+            res.write(data);
+            res.end();
+        }
+    });
 })
 
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
